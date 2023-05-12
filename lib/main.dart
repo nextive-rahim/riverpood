3ac535c7a7b55b05f0a1743337f1481333042bfc
Abdul Rahim <rahim.nextive@gmail.com>
2023-05-12 12:06:08 +0600

    Fixed : Removed debug banner.

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ref) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'Fake Commerce',
       routerConfig: ref.read(goRouterProvider),
       theme: ThemeData(
